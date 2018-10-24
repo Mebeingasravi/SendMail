7a060759af0b6ae924667f6059df2025183e34d9
vinod <vinodlkaradiya@gmail.com>
2018-10-24 19:29:55 +0530

    navbar added

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,9 @@ var express = require('express');
 var adminRounter =  express.Router();
 
 adminRounter.get('/',function(req,res){
-    res.send('<h1>hello admin</h1>');
+    res.render('admin/admin',{
+        title:'ADMIN'
+    });
 });
 
 module.exports = adminRounter;
